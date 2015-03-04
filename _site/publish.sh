3b54b7df787e4c94f2526329a93b8bdc42335368
Aaron Ott <aaron.ott@gmail.com>
2015-03-03 17:49:22 -0700

    committing

---
+++ _site/publish.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+JEKYLL=`which jekyll` || exit 0;
+
+NOW=$(date +"%m-%d-%Y")
+
+echo "Generating pages."
+
+$JEKYLL --no-server --auto > /dev/null &
+ 
+sleep 5;
+
+pgrep jekyll;
+
+pkill jekyll;
+
+git add .
+git commit -a -m "committing $now"
+
+# Edit references
+git update-ref refs/heads/master $(echo 'publishing changes to the blog.' | git commit-tree dev^{tree}:_site -p $(cat .git/refs/heads/master))
+
+git checkout master
+
+git push
+
+rm -rf _site
+echo "All done. Putting you back in dev."
+
+git checkout dev
